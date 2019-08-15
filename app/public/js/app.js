31cf88b8e779fd74e724bb0bf3865a0f01dbc128
emilybaum <emily.r.baum@gmail.com>
2019-08-15 01:27:28 -0400

    added a blue theme

---
+++ app/public/js/app.js
@@ -0,0 +1,21 @@
+// function animateCSS(element, animationName) {
+//     const node = document.querySelector(element)
+//     node.classList.add('animated', animationName)
+
+//     function handleAnimationEnd() {
+//         node.classList.remove('animated', animationName)
+//         node.removeEventListener('animationend', handleAnimationEnd)
+
+//         // if (typeof callback === 'function') callback()
+//     }
+
+//     node.addEventListener('animationend', handleAnimationEnd)
+// }
+
+// $(".text-enhance").hover(function () {
+//     animateCSS('.text-enhance', 'shake')
+// })
+
+// function test() {
+//     alert ("this is the call back")
+// }
