fa4222a57b74bda8bc2cf6a123919527b989041a
emilybaum <emily.r.baum@gmail.com>
2019-08-16 18:55:02 -0400

    changed the animated function to look for ID and now the individual elements are moving

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,35 +1,63 @@
-function animateCSS(element, animationName) {
-    var node = document.querySelector(element)
-    console.log(node)
+// function animateCSS(element, animationName) {
+//     var node = document.querySelector(element)
+//     console.log(node)
+//     node.classList.add('animated', animationName)
+
+//     function handleAnimationEnd() {
+//         node.classList.remove('animated', animationName)
+//         node.removeEventListener('animationend', handleAnimationEnd)
+
+//         if (typeof callback === 'function') test()
+//     }
+
+//     node.addEventListener('animationend', handleAnimationEnd)
+// }
+
+// // ENTRY FOR ANIMATION ON LETTERS
+$(".text-enhance").hover(function () {
+    console.log("hover")
+    var thisElement = $(this).attr("id");
+    // console.log(thisElement)
+    // // animateCSS(thisElement, 'shake')
+    // // const element = document.querySelector('.my-element')
+    // thisElement.addClass('animated', 'bounceOutLeft')
+    animateCSS(thisElement, 'rubberBand')
+})
+
+
+
+function animateCSS(element, animationName, callback) {
+    console.log("animate")
+    // var thisElement = $("#"+ element)
+    const node = document.getElementById(element)
     node.classList.add('animated', animationName)
 
     function handleAnimationEnd() {
         node.classList.remove('animated', animationName)
         node.removeEventListener('animationend', handleAnimationEnd)
 
-        if (typeof callback === 'function') test()
+        if (typeof callback === 'function') callback()
     }
 
     node.addEventListener('animationend', handleAnimationEnd)
 }
 
-// ENTRY FOR ANIMATION ON LETTERS
-// $(".text-enhance").hover(function () {
-//     thisElement = $(this).attr("id");
-//     console.log(thisElement)
-//     animateCSS(thisElement, 'shake')
-// })
 
-function test() {
-    alert ("this is the call back")
+function callback() {
+    console.log("callback successful")
 }
 
+
+
+
+// ===============================
 // bootstrap modal
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
 })
 
-$("projects-display").on("hover", function () {
-    alert("hover")
+// PORTFOLIO
+$(".projects-display").on("hover", function () {
     $(this).removeClass("hidden")
+    $(this).addClass("animated bounceIn")
 })
