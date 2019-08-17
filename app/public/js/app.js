bdc5a9f8d81ac9ee8a9e185bb1917133ba3c76a9
emilybaum <emily.r.baum@gmail.com>
2019-08-17 16:45:51 -0400

    made the words dynamically display

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,45 +1,82 @@
+$(document).ready(function () {
 
-// // ENTRY FOR ANIMATION ON LETTERS
-$(".text-animation").hover(function () {
-    console.log("hover")
-    var thisElement = $(this).attr("id");
-    animateCSS(thisElement, 'rubberBand', callback)
-})
+    // function pageLoadAnimate() {
 
+    // }
+    
+    // function makeVisible() {
+    //     $(".eb-hidden").each(function (i, value) {
+    //         // console.log(value)
+    //         // thisElement = value.attr("id")
+        
+    //         setTimeout(function () { 
+    //             // console.log(thisElement)
+    //         $("#this" + [i]).removeClass("eb-hidden")
 
+    //         }, 1000);
+    //     })
+    // }
+    // makeVisible()
 
-function animateCSS(element, animationName, callback) {
-    console.log("animate")
-    const node = document.getElementById(element)
-    node.classList.add('animated', animationName)
+    function makeVisible() {
+        console.log("make visible")
+        for (var j = 1; j <= 12; j++) {
+            thisElement = $("#this" + [j])
+            console.log("the element: " + thisElement)
+            otherFunction(j)
+        }
+    }
+    makeVisible()
 
-    function handleAnimationEnd() {
-        node.classList.remove('animated', animationName)
-        node.removeEventListener('animationend', handleAnimationEnd)
-
-        if (typeof callback === 'function') callback()
+    function otherFunction(value) {
+        setTimeout(function () {
+            $("#this" + [value]).removeClass("eb-hidden")
+            
+        }, 100*value);
     }
 
-    node.addEventListener('animationend', handleAnimationEnd)
-}
 
-
-function callback() {
-    console.log("callback successful")
-}
-
+    // // ENTRY FOR ANIMATION ON LETTERS
+    $(".text-animation").hover(function () {
+        var thisElement = $(this).attr("id");
+        animateCSS(thisElement, 'rubberBand', callback)
+    })
 
 
 
-// ===============================
-// bootstrap modal
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-})
+    function animateCSS(element, animationName, callback) {
+        const node = document.getElementById(element)
+        node.classList.add('animated', animationName)
 
-// PORTFOLIO
-$(".projects-display").on("hover", function () {
-    $(this).removeClass("hidden")
-    $(this).addClass("animated bounceIn")
-})
+        function handleAnimationEnd() {
+            node.classList.remove('animated', animationName)
+            node.removeEventListener('animationend', handleAnimationEnd)
 
+            if (typeof callback === 'function') callback()
+        }
+
+        node.addEventListener('animationend', handleAnimationEnd)
+    }
+
+
+    function callback() {
+        console.log("callback successful")
+    }
+
+
+
+
+    // ===============================
+    // bootstrap modal
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus')
+    })
+
+    // PORTFOLIO
+    $(".projects-display").on("hover", function () {
+        $(this).removeClass("hidden")
+        $(this).addClass("animated bounceIn")
+    })
+
+
+});
